06c18b3ce31e12d320ba8dc2c58365716f4873c6
Edward <op_230@mail.ru>
2021-04-07 23:42:31 +0300

    initial

--- main_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.models import User, Group
-
-from main_app.models import Document
-
-admin.site.unregister(User)
-admin.site.unregister(Group)
-
-admin.site.register(Document)
